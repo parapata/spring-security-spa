a4fa217fede277dfc637c279001658e533ebfa2a
parapata <parapata@users.noreply.github.com>
2018-02-10 23:38:28 +0900

    Update to Spring-Boot Ver.2.0.0.RC1

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # spring-security-react
+
 Spring-Security + Reactサンプルプログラム
 
 ## ログイン情報
+
 user/user
+
+
+-------------
+
+## 修正履歴
+
+
+### 2018-02-10
+- Spring-Boot Ver.2.0.0.RC1へ変更
+
+### 2017-10-01
+- 新規登録
--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -36,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password("user")
+                .password("$2a$10$EQzbsy3Pfmvs/9Ox2cGNseD8PCeN.q.EwKJ5e.47yhCmOtfL/rVHO")
                 .roles("USER");
     }
 
@@ -81,6 +84,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 response.sendRedirect("/");
             }
         };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
