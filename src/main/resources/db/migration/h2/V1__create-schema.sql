a585f83911b2105aa5906a92417ad1f09bc23b55
parapata <parapata@users.noreply.github.com>
2018-02-15 01:11:22 +0900

    Add: users schema

--- src/main/resources/db/migration/h2/V1__create-schema.sql
+++ src/main/resources/db/migration/h2/V1__create-schema.sql
@@ -1,5 +1,13 @@
 CREATE
-    TABLE customer(
+    TABLE users(
+        user_id VARCHAR(16) PRIMARY KEY
+        ,password VARCHAR(100)
+        ,first_name VARCHAR(30)
+        ,last_name VARCHAR(30)
+        ,email VARCHAR(100)
+    );
+CREATE
+    TABLE customers(
         id INT PRIMARY KEY AUTO_INCREMENT
         ,first_name VARCHAR(30)
         ,last_name VARCHAR(30)
--- src/main/resources/db/migration/h2/V2__import-initial-data.sql
+++ src/main/resources/db/migration/h2/V2__import-initial-data.sql
@@ -1,4 +1,7 @@
-INSERT INTO customer (first_name, last_name) VALUES ('Nobita', 'Nobi');
-INSERT INTO customer (first_name, last_name) VALUES ('Takeshi', 'Goda');
-INSERT INTO customer (first_name, last_name) VALUES ('Suneo', 'Honekawa');
-INSERT INTO customer (first_name, last_name) VALUES ('Shizuka', 'Minamoto');
+INSERT INTO users (user_id, password, first_name, last_name, email) VALUES ('user', '$2a$10$EQzbsy3Pfmvs/9Ox2cGNseD8PCeN.q.EwKJ5e.47yhCmOtfL/rVHO', 'Ichiro', 'Suzuki', 'ichiro@suzuki.co.jp');
+
+INSERT INTO customers (first_name, last_name) VALUES ('Nobita', 'Nobi');
+INSERT INTO customers (first_name, last_name) VALUES ('Takeshi', 'Goda');
+INSERT INTO customers (first_name, last_name) VALUES ('Suneo', 'Honekawa');
+INSERT INTO customers (first_name, last_name) VALUES ('Shizuka', 'Minamoto');
+
